47392ca23a7d4980a8816c2872f1019d3179f175
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-07-13 16:51:56 +0700

    v6

--- api/app.js
+++ api/app.js
@@ -9,13 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/pemasukan', require('../routes/pemasukan'));
-app.use('/api/pengeluaran', require('../routes/pengeluaran'));
-app.use('/api/laporan', require('../routes/laporan'));
-app.use('/api/laporan-pdf', require('../routes/laporanPdf'));
-app.use('/api/auth', require('../routes/auth'));
-app.use('/api/pengajuan', require('../routes/pengajuan'));
+app.use('/api/pemasukan', require('./routes/pemasukan'));
+app.use('/api/pengeluaran', require('./routes/pengeluaran'));
+app.use('/api/laporan', require('./routes/laporan'));
+app.use('/api/laporan-pdf', require('./routes/laporanPdf'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/pengajuan', require('./routes/pengajuan'));
 
-app.get('/', (req, res) => res.send('API Bendahara 17-an Aktif'));
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel Express!');
+});
 
 module.exports = serverless(app); // ✅ export hanya di bawah
