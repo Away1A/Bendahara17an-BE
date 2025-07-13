3e060887bcc02f7b0c4d50542f6c3bd1fcca54ac
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-07-13 17:42:40 +0700

    test

--- api/index.js
+++ api/index.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/pemasukan', require('../routes/pemasukan'));
-app.use('/api/pengeluaran', require('../routes/pengeluaran'));
-app.use('/api/laporan', require('../routes/laporan'));
-app.use('/api/laporan-pdf', require('../routes/laporanPdf'));
-app.use('/api/auth', require('../routes/auth'));
-app.use('/api/pengajuan', require('../routes/pengajuan'));
+// // Routes
+// app.use('/api/pemasukan', require('../routes/pemasukan'));
+// app.use('/api/pengeluaran', require('../routes/pengeluaran'));
+// app.use('/api/laporan', require('../routes/laporan'));
+// app.use('/api/laporan-pdf', require('../routes/laporanPdf'));
+// app.use('/api/auth', require('../routes/auth'));
+// app.use('/api/pengajuan', require('../routes/pengajuan'));
 
 app.get('/', (req, res) => {
   res.send('Hello from Vercel Express!');
