ae3bb67c9090b8af7d3d02ca466e612d629ba743
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-07-13 16:38:59 +0700

    vercel

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
+/** 1. SETUP EXPRESS NORMAL */
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
-// Import rute (nanti dibuat)
-const pemasukanRoutes = require('./routes/pemasukan');
-const pengeluaranRoutes = require('./routes/pengeluaran');
-const laporanRoutes = require('./routes/laporan');
-const laporanPdfRoutes = require('./routes/laporanPdf');
-const authRoutes = require('./routes/auth');
-const pengajuanRoutes = require('./routes/pengajuan')
+/** 2. ROUTES */
+app.use('/api/pemasukan', require('../routes/pemasukan'));
+app.use('/api/pengeluaran', require('../routes/pengeluaran'));
+app.use('/api/laporan', require('../routes/laporan'));
+app.use('/api/laporan-pdf', require('../routes/laporanPdf'));
+app.use('/api/auth', require('../routes/auth'));
+app.use('/api/pengajuan', require('../routes/pengajuan'));
 
-app.use('/api/pemasukan', pemasukanRoutes);
-app.use('/api/pengeluaran', pengeluaranRoutes);
-app.use('/api/laporan', laporanRoutes);
-app.use('/api/laporan-pdf', laporanPdfRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/pengajuan", pengajuanRoutes);
+app.get('/', (req, res) => res.send('API Bendahara 17‑an Aktif'));
 
-
-
-app.get('/', (req, res) => res.send('API Bendahara 17-an Aktif'));
-
-app.listen(PORT, () => console.log(`Server running di http://localhost:${PORT}`));
+/** 3. EXPORT AS SERVERLESS HANDLER */
+const serverless = require('serverless-http');
+module.exports = serverless(app);
